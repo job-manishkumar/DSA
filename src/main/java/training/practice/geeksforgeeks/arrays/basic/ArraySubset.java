19f416279001b40142d70fdd551225047bd6f66e
manish <job.manishkumar@gmail.com>
2024-10-18 19:24:30 +0530

    check two arrays are equal or not

---
+++ src/main/java/training/practice/geeksforgeeks/arrays/basic/ArraySubset.java
@@ -0,0 +1,14 @@
+package training.practice.geeksforgeeks.arrays.basic;
+
+public class ArraySubset {
+
+	public static String isSubset(int[] arr1,int[] arr2){
+
+		return null;
+	}
+
+	public static void main(String[] args) {
+		int[] a1 = {11, 7, 1, 13, 21, 3, 7, 3};
+		int[] a2 = {11, 3, 7, 1, 7};
+	}
+}
