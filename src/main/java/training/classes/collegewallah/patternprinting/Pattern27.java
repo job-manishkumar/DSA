ac494f978a946ec7565ada3d28cf1fca66216a7b
manish <job.manishkumar@gmail.com>
2024-10-18 19:26:16 +0530

    pattern program

---
+++ src/main/java/training/classes/collegewallah/patternprinting/Pattern27.java
@@ -0,0 +1,31 @@
+package training.classes.collegewallah.patternprinting;
+
+import java.util.Scanner;
+
+public class Pattern27 {
+
+	public static void printPattern(int row){
+
+		for(int i=0;i<row;i++){
+			for(int j=0;j<row-1-i;j++){
+				System.out.print(" ");
+			}
+			for(int j=0;j<=i;j++){
+				System.out.print(j+1);
+			}
+			for(int j=i;j>0;j--){
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+
+		int row = scanner.nextInt();
+		printPattern(row);
+		scanner.close();
+	}
+}
