4d042d5c3bd969dfdf9516eb309fdfa89af3147f
manish <job.manishkumar@gmail.com>
2024-10-21 15:03:04 +0530

    Peak element update of binary search

--- src/main/java/training/practice/geeksforgeeks/arrays/basic/PeakElement.java
+++ src/main/java/training/practice/geeksforgeeks/arrays/basic/PeakElement.java
@@ -3,6 +3,7 @@ package training.practice.geeksforgeeks.arrays.basic;
 public class PeakElement {
 
 
+	//approach 1
 	//TC->O(N) & SC->O(1)
 	public static int peakElement(int[] arr) {
 
@@ -23,10 +24,49 @@ public class PeakElement {
 		return -1;
 	}
 
+	//approach 2 using binary search
+	//TC -> (nlogn) and sc -> (1)
+	public static int peakElement_2(int[] arr){
+
+		if(arr.length==0){
+			return -1;
+		}
+		if(arr.length==1 || arr[0]>arr[1]){
+			return 0;
+		}
+		if(arr[arr.length-1]> arr[arr.length-2]){
+			return arr.length-1;
+		}
+
+		int lowIndex = 0;
+		int highIndex = arr.length-1;
+
+
+
+		while(lowIndex<=highIndex){
+			int mid = lowIndex + ((highIndex-lowIndex)/2);
+			if(arr[mid]>arr[mid-1] && arr[mid] > arr[mid+1]){
+				return mid;
+			}
+			else if(arr[mid-1]>arr[mid]){
+				highIndex = mid-1;
+			}
+			else{
+				lowIndex = mid+1;
+			}
+
+		}
+		return -1;
+	}
+
+
+
 	public static void main(String[] args) {
 		//int[] a = {1, 2, 3};
-		int[] a = {1, 1, 1, 2, 1, 1, 1};
-		System.out.println(peakElement(a));
+		//int[] a = {1, 1, 1, 2, 1, 1, 1};
+		//System.out.println(peakElement(a));
+		int[] a = {2,4,12,8,10,14,16,13};
+		System.out.println(peakElement_2(a));
 	}
 
 }
