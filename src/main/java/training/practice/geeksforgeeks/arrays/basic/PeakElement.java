7fb5e19dced685233a483c14e293aa5c548982bd
manish <job.manishkumar@gmail.com>
2024-10-21 22:08:03 +0530

    Peak element

--- src/main/java/training/practice/geeksforgeeks/arrays/basic/PeakElement.java
+++ src/main/java/training/practice/geeksforgeeks/arrays/basic/PeakElement.java
@@ -26,33 +26,30 @@ public class PeakElement {
 
 	//approach 2 using binary search
 	//TC -> (nlogn) and sc -> (1)
-	public static int peakElement_2(int[] arr){
+	public static int peakElement_2(int[] arr) {
 
-		if(arr.length==0){
+		if (arr.length == 0) {
 			return -1;
 		}
-		if(arr.length==1 || arr[0]>arr[1]){
+		if (arr.length == 1 || arr[0] > arr[1]) {
 			return 0;
 		}
-		if(arr[arr.length-1]> arr[arr.length-2]){
-			return arr.length-1;
+		if (arr[arr.length - 1] > arr[arr.length - 2]) {
+			return arr.length - 1;
 		}
 
 		int lowIndex = 0;
-		int highIndex = arr.length-1;
+		int highIndex = arr.length - 1;
 
 
-
-		while(lowIndex<=highIndex){
-			int mid = lowIndex + ((highIndex-lowIndex)/2);
-			if(arr[mid]>arr[mid-1] && arr[mid] > arr[mid+1]){
+		while (lowIndex <= highIndex) {
+			int mid = lowIndex + ((highIndex - lowIndex) / 2);
+			if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]) {
 				return mid;
-			}
-			else if(arr[mid-1]>arr[mid]){
-				highIndex = mid-1;
-			}
-			else{
-				lowIndex = mid+1;
+			} else if (arr[mid - 1] > arr[mid]) {
+				highIndex = mid - 1;
+			} else {
+				lowIndex = mid + 1;
 			}
 
 		}
@@ -60,12 +57,11 @@ public class PeakElement {
 	}
 
 
-
 	public static void main(String[] args) {
 		//int[] a = {1, 2, 3};
 		int[] a = {1, 1, 1, 2, 1, 1, 1};
 		System.out.println(peakElement(a));
-		 a = new int[]{2,4,12,8,10,14,16,13};
+		a = new int[]{2, 4, 12, 8, 10, 14, 16, 13};
 		System.out.println(peakElement_2(a));
 	}
 
