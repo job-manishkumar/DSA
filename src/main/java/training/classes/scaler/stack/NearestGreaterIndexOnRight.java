dab407ec2ef0bbeec1a53bebee68aaf820719df3
manish <job.manishkumar@gmail.com>
2024-12-21 19:59:42 +0530

    Get the Nearest Greater index on the right side of array.

---
+++ src/main/java/training/classes/scaler/stack/NearestGreaterIndexOnRight.java
@@ -0,0 +1,49 @@
+package training.classes.scaler.stack;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class NearestGreaterIndexOnRight {
+
+	//approach 1 using loop
+	//tc -> O(n2) and sc ->O(n)
+	public static int[] getNearestGreaterIndex(int[] arr) {
+		int[] res = new int[arr.length];
+		for (int i = arr.length - 1; i >= 0; i--) {
+			res[i] = -1;
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[j] > arr[i]) {
+					res[i] = j;
+					break;
+				}
+			}
+		}
+		return res;
+	}
+
+	//approach 1 using stack
+	//tc -> O(n) and sc ->O(n)
+	public static int[] getNearestGreaterIndex_2(int[] arr) {
+		Stack<Integer> st = new Stack<>();
+		int[] res = new int[arr.length];
+		for (int i = arr.length - 1; i >= 0; i--) {
+			while (!st.isEmpty() && arr[st.peek()] < arr[i]) {
+				st.pop();
+			}
+			if (st.isEmpty()) {
+				res[i] = -1;
+			} else {
+				res[i] = st.peek();
+			}
+			st.push(i);
+		}
+		return res;
+	}
+
+	public static void main(String[] args) {
+		int[] arr = {4, 6, 10, 11, 7, 8, 3, 5};
+		System.out.println(Arrays.toString(getNearestGreaterIndex(arr)));
+		System.out.println("============");
+		System.out.println(Arrays.toString(getNearestGreaterIndex_2(arr)));
+	}
+}
