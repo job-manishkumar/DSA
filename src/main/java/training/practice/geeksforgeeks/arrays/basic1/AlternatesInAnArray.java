d3c31c8ee86192082369c2fd0de267f6911c40f0
manish <job.manishkumar@gmail.com>
2025-01-08 10:59:38 +0530

    get alternative number from array.

---
+++ src/main/java/training/practice/geeksforgeeks/arrays/basic1/AlternatesInAnArray.java
@@ -0,0 +1,22 @@
+package training.practice.geeksforgeeks.arrays.basic1;
+
+import java.util.ArrayList;
+
+/*You are given an array arr[], the task is to return a list elements of arr in alternate order (starting from index
+0).*/
+public class AlternatesInAnArray {
+
+	public static ArrayList<Integer> getAlternativeNumber(int arr[]) {
+		ArrayList<Integer> list = new ArrayList<>();
+		for (int i = 0; i < arr.length; i += 2) {
+			list.add(arr[i]);
+		}
+		return list;
+	}
+
+
+	public static void main(String[] args) {
+		int[] arr = {1, 2, 3, 4};
+		System.out.println(getAlternativeNumber(arr));
+	}
+}
