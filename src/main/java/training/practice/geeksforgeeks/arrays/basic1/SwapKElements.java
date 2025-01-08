50a529a4460b9e1937ab151f0267243711f68c9d
manish <job.manishkumar@gmail.com>
2025-01-08 15:57:29 +0530

    swap kth index element

---
+++ src/main/java/training/practice/geeksforgeeks/arrays/basic1/SwapKElements.java
@@ -0,0 +1,26 @@
+package training.practice.geeksforgeeks.arrays.basic1;
+
+import java.util.Arrays;
+import java.util.List;
+
+/*Given an array arr, swap the kth element from the beginning with the kth element from the end.
+
+Note: 1-based indexing is followed.*/
+public class SwapKElements {
+
+	// tc ->O(1) and sc  ->(1)
+	public static void swapKElement(List<Integer> list, int k) {
+		int startIndex = k - 1;
+		int endIndex = list.size() - k;
+		int temp = list.get(startIndex);
+		list.set(startIndex, list.get(endIndex));
+		list.set(endIndex, temp);
+	}
+
+	public static void main(String[] args) {
+		List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
+		int k = 3;
+		swapKElement(list, k);
+		System.out.println(list);
+	}
+}
