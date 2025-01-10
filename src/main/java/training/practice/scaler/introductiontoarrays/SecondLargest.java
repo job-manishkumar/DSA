8d245a079a928058b6d19197202088eff38c1327
manish <job.manishkumar@gmail.com>
2025-01-10 12:46:54 +0530

    Second Largest

--- src/main/java/training/practice/scaler/introductiontoarrays/SecondLargest.java
+++ src/main/java/training/practice/scaler/introductiontoarrays/SecondLargest.java
@@ -9,7 +9,7 @@ public class SecondLargest {
 		int max1 = Integer.MIN_VALUE;
 		int max2 = Integer.MIN_VALUE;
 		for (int i = 0; i < arr.length; i++) {
-			if (max1 < arr[i]) {Second Largest
+			if (max1 < arr[i]) {
 				max2 = max1;
 				max1 = arr[i];
 			}
